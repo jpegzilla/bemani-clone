cf9360a532079db11ce88da30b59132739e6fd20
jpegzilla <eris@jpegzilla.com>
2019-12-08 04:57:22 -0500

    updated audio player classes

--- js/modules/audioplayer.mjs
+++ js/modules/audioplayer.mjs
@@ -1,19 +1,36 @@
 class Sound {
   constructor(path) {
     this.path = path;
+
+    // what timestamp to start at
+    this.startTime = 0;
+
+    // the timestamp when the sound was stopped
+    this.stopTime = 0;
+
+    // the timestamp when the sound is paused.
+    // if the audio is paused, then the startTime is set
+    // equal to the pauseTime.
+    this.pauseTime = 0;
+
+    console.log("sound:", { path });
   }
+
+  play() {}
+
+  pause() {}
+
+  stop() {}
 }
 
 export class SoundEffect extends Sound {
   constructor(path) {
     super(path);
-    this.path = path;
   }
 }
 
 export class Music extends Sound {
   constructor(path) {
     super(path);
-    this.path = path;
   }
 }
