f84ee3a203ebdcb7f8d70571c9f2669d6e4b7a97
jpegzilla <eris@jpegzilla.com>
2019-12-07 19:47:19 -0500

    updated default logging function

--- js/modules/logger.mjs
+++ js/modules/logger.mjs
@@ -2,8 +2,12 @@ export const gameLog = msg => {
   return console.log("[BEMANI ★ INGAME]", JSON.stringify(msg, null, 2));
 };
 
-export const log = (msg, prefix = "[BEMANI ★ CLONE]") => {
+export const jsonLog = (msg, prefix = "[BEMANI ★ CLONE]") => {
   return console.log(prefix, JSON.stringify(msg, null, 2));
+};
+
+export const log = (msg, prefix = "[BEMANI ★ CLONE]") => {
+  return console.log(prefix, msg);
 };
 
 export const navLog = msg => {
