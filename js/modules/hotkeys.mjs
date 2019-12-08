8ba16b83df5a12bcd14acc3281da227bccf1cb13
jpegzilla <eris@jpegzilla.com>
2019-12-07 19:46:39 -0500

    added new hotkeys

--- js/modules/hotkeys.mjs
+++ js/modules/hotkeys.mjs
@@ -1,5 +1,6 @@
 import cheatcode from "./../libs/cheatcodes.mjs";
 import { log } from "./logger.mjs";
+import { GLOBAL_FLAGS, GLOBAL_SETTINGS, state } from "./statemanager.mjs";
 
 export default () => {
   // konami code, just for testing
@@ -10,6 +11,22 @@ export default () => {
     }
   );
 
+  const openSettings = new cheatcode("ctrl, shift, s", () => {
+    log("opening settings");
+  });
+
+  // for development only - clear storage and reload
+  const clearAndReload = new cheatcode("ctrl, alt, r", () => {
+    window.localStorage.clear();
+    location.reload(true);
+  });
+
+  // for development: show global flags when s is pressed
+  const showGlobalObject = new cheatcode("ctrl, alt, s", () => {
+    console.log({ GLOBAL_FLAGS, GLOBAL_SETTINGS, state });
+  });
   // run the start function on each hotkey
-  [konami].forEach(c => c.start());
+  state.cheatcodes = [konami, openSettings, clearAndReload, showGlobalObject];
+
+  state.cheatcodes.forEach(c => c.start());
 };
