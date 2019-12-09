d89a0f597818e98b69342f35670bb930596f3a86
jpegzilla <eris@jpegzilla.com>
2019-12-08 19:25:54 -0500

    updated event utils

--- js/modules/customevents.mjs
+++ js/modules/customevents.mjs
@@ -2,5 +2,11 @@ export const navigationEvent = location =>
   new CustomEvent("navigationEvent", {
     detail: {
       location: location
-    }
+    },
+    bubbles: false,
+    cancelable: false
   });
+
+export const fireEvent = ev => document.dispatchEvent(ev);
+
+// ex: fireEvent(navigationEvent("start screen"))
