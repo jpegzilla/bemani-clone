c5b302ad10be6803f8946c99f34df403b9eb55ff
jpegzilla <eris@jpegzilla.com>
2019-12-08 19:21:45 -0500

    started on sound effect work

--- js/modules/effects.mjs
+++ js/modules/effects.mjs
@@ -1,8 +1,19 @@
 // // // contains: interfaces for creating visual effects and audio effects.
+import { typedElementArray } from "./utils.mjs";
+import { SoundEffect } from "./audioplayer.mjs";
 
 // // visual effects
 
 // // audio effects
+// link hover effects
+typedElementArray("a").forEach(a => {
+  const linkToSound = "";
+  const sound = new SoundEffect(linkToSound);
+
+  a.addEventListener("mouseover", () => {
+    sound.play();
+  });
+});
 
 // // main menu/ui audio effects
 
